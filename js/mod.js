bf867f13b40311291f5ec2d660169cc881e2c47c
BanaCubed <162780442+BanaCubed@users.noreply.github.com>
2024-05-03 13:00:52 +1200

    Old version fix

--- js/mod.js
+++ js/mod.js
@@ -178,6 +178,10 @@ function maxTickLength() {
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
 // you can cap their current resources with this.
 function fixOldSave(oldVersion){
+	if(oldVersion == "0.3.2") {
+		player.DM = {}
+		player.EM = {}
+	}
 }
 
 function achievement33() {
